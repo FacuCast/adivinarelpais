5403977a803681fbfc1fff7a63e7cb8d33585b2a
FacuCast <82911322+FacuCast@users.noreply.github.com>
2023-03-06 23:34:52 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let modalWrapper = document.createElement("div");
 let pais = []; //an array to hold all the alpha3Code for all countries
 let prue = [];
+let idioma = [];
+let capital = [];
+let puntos = 0;
+let pistas = 0;
+let region = [];
 let i = 0;
 
 const fetchCountry = async (event) => {
@@ -13,8 +18,13 @@ const fetchCountry = async (event) => {
 
 			data.forEach(element => {
 				pais[i] = element.name.common;
+				region[i] = element.region;
+				idioma[i] = element.region;
+				capital[i] = element.region;
+
 				i++;
 			})
+
 			Adivina();
 		})
 
@@ -26,7 +36,9 @@ function getRandomInt(max) {
 
 function Adivina() {
 	numero = getRandomInt(130);
-
+	region[300] = region[numero];
+	idioma[300] = idioma[i];
+	capital[300] = capital[i];
 	//old api that was changed - `https://restcountries.eu/rest/v2/all `;
 	const apiEndpoint = `https://restcountries.com/v3.1/name/${pais[numero]}`
 
@@ -37,6 +49,7 @@ function Adivina() {
 			data.forEach(element => {
 				prue[1] = element;
 				pais[1] = element.name.common;
+
 			})
 			bandera(prue[1]);
 		})
@@ -83,18 +96,64 @@ function editDistance(s1, s2) {
 	return costs[s2.length];
 }
 
+
+function pista() {
+	pistas++
+
+	switch (pistas) {
+		case 1:
+			var borrardiv = document.getElementById("text1").lastChild;
+			document.getElementById("text1").removeChild(borrardiv);
+			const newElement = document.createElement("div");
+			newElement.classList.add("div");
+			newElement.innerHTML = `<h3 id="h2">El pais es de la Region: ${region[300]} <h3>`;
+			document.querySelector("#text1").appendChild(newElement);
+
+			puntos -= 10;
+			break;
+		case 2:
+			var borrardiv = document.getElementById("text1").lastChild;
+			document.getElementById("text1").removeChild(borrardiv);
+			const newElement1 = document.createElement("div");
+			newElement1.classList.add("div");
+			newElement1.innerHTML = `<h3 id="h2">El pais habla el idioma: ${idioma[300]} <h3>`;
+			document.querySelector("#text1").appendChild(newElement1);
+
+			puntos -= 20;
+
+			break;
+		case 3:
+			var borrardiv = document.getElementById("text1").lastChild;
+			document.getElementById("text1").removeChild(borrardiv);
+			const newElement2 = document.createElement("div");
+			newElement2.classList.add("div");
+			newElement2.innerHTML = `<h3 id="h2">La capital es: ${capital[300]} <h3>`;
+			document.querySelector("#text1").appendChild(newElement2);
+
+			puntos -= 30;
+			break;
+		default:
+			break;
+	}
+}
+
 function mostrar() {
 	let inputValue = document.getElementById("paiss").value;
 	inputValue = inputValue.toLowerCase();
 	pais[1] = pais[1].toLowerCase();
 
 	if (inputValue == pais[1]) {
-		var borrardiv = document.getElementById("text1").lastChild;
-		document.getElementById("text1").removeChild(borrardiv);
+
+		document.getElementById('text1').innerHTML = '';
+	
 		const newElement = document.createElement("div");
 		newElement.classList.add("div");
-		newElement.innerHTML = `<h2>Felicitaciones!!!!</h2>`;
+		puntos += 100;
+		newElement.innerHTML = `<h2>Puntuacion: ${puntos}</h2>`;
 		document.querySelector("#modal").appendChild(newElement);
+
+		document.getElementById('Martin').innerHTML = '';
+		Adivina();
 	} else {
 		var borrardiv = document.getElementById("text1").lastChild;
 		document.getElementById("text1").removeChild(borrardiv);
@@ -105,7 +164,8 @@ function mostrar() {
 
 		const newElement = document.createElement("div");
 		newElement.classList.add("div");
-		newElement.innerHTML = `<h2>Porcentaje de comparacion: ${similar} <h2> `;
+		newElement.innerHTML = `<h2>Porcentaje de comparacion: ${similar} <h2> \n
+		<button onclick="pista()">Pista</button>`;
 		document.querySelector("#text1").appendChild(newElement);
 
 	}
@@ -117,7 +177,7 @@ function bandera(element) {
 	newElement.classList.add("div");
 	newElement.innerHTML = `					
 					
-				<div class="country-details">		
+				<div class="country-details" id="Martin">		
 					<img class="country-details-img" src= ${element.flags.svg} alt="sos hacker ${element.name.common} " tabindex=0>
 					<h1 style="text-align: center;">¿De cual pais es esta bandera?</h1>
 					<input type="text" name="paiss" id="paiss">
